035861eb57d951ebd4ff8a371e19f894038a6aa7
Meander River <ieinstein@163.com>
2016-03-17 21:36:22 +0800

    Initial Commit

---
+++ MultiValuePickerController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiValuePickerController
+//
+//  Created by cc on 16/3/17.
+//  Copyright © 2016年 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MultiValuePickerController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultiValuePickerController
+//
+//  Created by cc on 16/3/17.
+//  Copyright © 2016年 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
